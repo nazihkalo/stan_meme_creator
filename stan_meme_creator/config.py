08467798823a51bb653956e4f4ffb965ba4af95d
Nazih Kalo <nazih@phantom.app>
2024-11-12 07:46:49 -0500

    remove uneeded outpit dir

--- stan_meme_creator/config.py
+++ stan_meme_creator/config.py
@@ -3,11 +3,11 @@ from pathlib import Path
 # Base paths
 ROOT_DIR = Path(__file__).parent
 STATIC_DIR = ROOT_DIR / "static"
-OUTPUT_DIR = ROOT_DIR / "output"
+# OUTPUT_DIR = ROOT_DIR / "output"
 
 # Template directories
 MEME_TEMPLATES_DIR = STATIC_DIR / "meme_templates"
 OVERLAY_TEMPLATES_DIR = STATIC_DIR / "overlay_templates"
 
 # Ensure output directory exists
-OUTPUT_DIR.mkdir(exist_ok=True) 
+# OUTPUT_DIR.mkdir(exist_ok=True) 
